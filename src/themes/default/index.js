1ddfa18ef4a8e0f058c099c7e7ba3c5bc36b8414
David Dyke <david.dyke@skilltrees.io>
2020-09-15 10:11:11 +0100

    added theming

---
+++ src/themes/default/index.js
@@ -0,0 +1,16 @@
+import {createMuiTheme} from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary:  { main: '#4CAF50' },
+    secondary: { main: '#5d4037' }
+  },
+  typography: {
+    h6: {
+      fontWeight: 200,
+      fontSize: "1.15rem"
+    }
+  }
+});
+export default theme;
